0fedcd61065b7eea91ed32104e81d54550e888a0
umer112 <umerfarooq889k@gmail.com>
2022-10-06 03:53:43 +0500

    New Layout Structured

--- src/components/Layout/TopBar.jsx
+++ src/components/Layout/TopBar.jsx
@@ -1,9 +1,12 @@
-import { Button, Menu, MenuItem } from '@mui/material'
+import { Badge, Button, Menu, MenuItem } from '@mui/material'
 import { Box } from '@mui/system';
 import React from 'react'
 
 import SettingsIcon from '@mui/icons-material/Menu';
+import MailIcon from '@mui/icons-material/Mail';
 
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import IconButton from '@mui/material/IconButton';
 
 
 function TopBar() {
@@ -17,23 +20,27 @@ function TopBar() {
     };
     return (
         <div className='primary-topbar'>
-            <div className="top-bar--content shadow-md pt-[25px] pl-5 pb-3">
+            <div className="top-bar--content shadow-md pt-[25px] pr-10 pl-5 pb-3">
                 <Box className='flex justify-between items-center '>
                     <div>
                         Topbar
                     </div>
-                    <div >
-                        <Button
-                            id="basic-button"
-                            aria-controls={open ? 'basic-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
+                    <div className='flex gap-8   items-center' >
+                        <Badge badgeContent={4} color="primary">
+                            <NotificationsIcon color="action" />
+                        </Badge>
+                        <Badge badgeContent={4} color="primary">
+                            <MailIcon color="action" />
+                        </Badge>
+                        <IconButton
+                            id="basic-IconButton"
+
                             onClick={handleClick}
-                            variant='text'
+
 
                         >
                             <SettingsIcon className='settings-icon' />
-                        </Button>
+                        </IconButton>
                         <Menu
                             id="basic-menu"
                             anchorEl={anchorEl}
