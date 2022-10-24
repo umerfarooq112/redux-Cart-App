f0287f74d39e2871a915f7f340bbc75c32dbcf5b
umer112 <umerfarooq889k@gmail.com>
2022-10-24 14:12:22 +0500

    new Table Added

--- src/Pages/Items/index.jsx
+++ src/Pages/Items/index.jsx
@@ -1,4 +1,4 @@
-import { Paper, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from "@mui/material";
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -10,8 +10,24 @@ import Modal from '@mui/material/Modal';
 import { FormLabel } from '@mui/material';
 import { useFormik } from "formik";
 import Layout from "../../Layout";
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+
 function Items() {
     const [Data, setTableData] = useState([])
+
     const formik = useFormik({
         initialValues: {
             name: '',
@@ -32,32 +48,69 @@ function Items() {
         return AxiosClient.get('items/getAllItems').then(res => {
             console.log(res.data, 'get All items')
             setTableData(res.data)
+
         })
     }
     useEffect(() => {
+
         RefetchData()
+
     }, [])
 
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 500,
-        bgcolor: 'background.paper',
-        borderRadius: '12px',
-        boxShadow: 24,
-        p: 4,
-    };
+
 
     return <>
         <Layout>
-            <div className="items-wrapper pl-5 pt-5">
+            <div className="items-wrapper pl-5 pt-5 pr-5">
                 <Box>
-                    <p>Items, ipsum dolor sit amet consectetur adipisicing elit. Unde, fuga?</p>
+                    <Button onClick={handleOpen}>Open modal</Button>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2">
+                                Text in a modal
+                            </Typography>
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                            </Typography>
+                        </Box>
+                    </Modal>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Item Id</TableCell>
+                                    <TableCell align="right">Item Name</TableCell>
+                                    <TableCell align="right">Price</TableCell>
+                                    <TableCell align="right">Description</TableCell>
+
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {Data.map((row) => (
+                                    <TableRow
+                                        key={row.name}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="right">{row.name}</TableCell>
+                                        <TableCell align="right">{row.price}</TableCell>
+                                        <TableCell align="right">{row.description}</TableCell>
+
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </Box>
             </div>
         </Layout>
